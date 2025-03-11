7dba6771b0bba5077e1c3f073d5beb754b96d0f3
unknown <mshahzadwaris92@gmail.com>
2025-03-11 20:58:14 +0500

    Fixing

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -5,3 +6,8 @@ app = FastAPI()
 @app.get("/")
 def home():
     return {"message": "Hello, World! Deployment successful"}
+
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.getenv("PORT", 8000))  # Use Railway's PORT variable
+    uvicorn.run(app, host="0.0.0.0", port=port)
